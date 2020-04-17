c9b1aee06df42717daac403ab8bce302380873f2
Joseph T Lyons <JosephTLyons@gmail.com>
2020-04-16 20:42:21 -0400

    Some cleaning up and symlinks can now be filtered correctly from files

--- src/lib.rs
+++ src/lib.rs
@@ -6,23 +6,24 @@ mod orderable_set;
 use orderable_set::OrderableSet;
 
 pub enum Filter {
-    Item(Item),
-    Visibility(Visibility),
+    Item(ItemFilter),
+    Visibility(VisibilityFilter),
 }
 
-pub enum Item {
+pub enum ItemFilter {
     Directory,
     File,
     Symlink,
 }
 
-pub enum OrderBy {
+pub enum OrderByFilter {
+    // Just sort all of them ascending, then the reverse can be applied
     Extension,
     Name,
     Size,
 }
 
-pub enum Size {
+pub enum SizeFilter {
     Bytes,
     Kilobytes,
     Megatbytes,
@@ -30,7 +31,7 @@ pub enum Size {
     Terabytes,
 }
 
-pub enum Visibility {
+pub enum VisibilityFilter {
     Hidden,
     Visible,
 }
@@ -59,28 +60,31 @@ impl FileSet {
         }
     }
 
-    fn filter_by_item(&mut self, directory_entry_type: Item) -> OrderableSet<PathBuf> {
+    fn filter_by_item(&mut self, item_filter: ItemFilter) -> OrderableSet<PathBuf> {
         let mut orderable_set: OrderableSet<PathBuf> = OrderableSet::new();
 
-        match directory_entry_type {
-            Item::Directory => {
+        match item_filter {
+            ItemFilter::Directory => {
                 for path_buf in self.orderable_set.to_vec() {
-                    if path_buf.metadata().unwrap().is_dir() {
+                    if path_buf.metadata().unwrap().file_type().is_dir() {
                         orderable_set.push(path_buf).unwrap()
                     }
                 }
             }
-            Item::File => {
+            ItemFilter::File => {
                 for path_buf in self.orderable_set.to_vec() {
-                    if path_buf.metadata().unwrap().is_file() {
+                    if path_buf.symlink_metadata().unwrap().file_type().is_file() {
                         orderable_set.push(path_buf).unwrap()
                     }
                 }
             }
-            Item::Symlink => {
+            ItemFilter::Symlink => {
                 for path_buf in self.orderable_set.to_vec() {
-                    if !path_buf.metadata().unwrap().is_dir()
-                        && !path_buf.metadata().unwrap().is_file()
+                    if path_buf
+                        .symlink_metadata()
+                        .unwrap()
+                        .file_type()
+                        .is_symlink()
                     {
                         orderable_set.push(path_buf).unwrap()
                     }
@@ -91,11 +95,14 @@ impl FileSet {
         orderable_set
     }
 
-    pub fn filter_by_visibility(&mut self, visibility_type: Visibility) -> OrderableSet<PathBuf> {
+    pub fn filter_by_visibility(
+        &mut self,
+        visibility_filter: VisibilityFilter,
+    ) -> OrderableSet<PathBuf> {
         let mut orderable_set: OrderableSet<PathBuf> = OrderableSet::new();
 
-        match visibility_type {
-            Visibility::Hidden => {
+        match visibility_filter {
+            VisibilityFilter::Hidden => {
                 // let x: Vec<PathBuf> = self
                 //     .orderable_set
                 //     .to_vec()
@@ -115,7 +122,7 @@ impl FileSet {
                     }
                 }
             }
-            Visibility::Visible => {
+            VisibilityFilter::Visible => {
                 for path_buf in self.orderable_set.to_vec() {
                     if !path_buf
                         .file_name()
@@ -132,11 +139,7 @@ impl FileSet {
         orderable_set
     }
 
-    // pub fn exclude(&mut self, filer_type: FilterType) -> FileSet {
-    //     // let x = self.filter(filer_type).orderable_set;
-    //     // self.orderable_set.difference(x.clone());
-    //
-    //     self
+    // pub fn exclude(&mut self, filter: Filter) -> FileSet {
     // }
 
     pub fn reverse(&mut self) -> FileSet {
@@ -145,18 +148,14 @@ impl FileSet {
         FileSet { orderable_set }
     }
 
-    pub fn to_vec(&self) -> Option<Vec<PathBuf>> {
-        if self.orderable_set.to_vec().is_empty() {
-            return None;
-        }
-
+    pub fn to_vec(&self) -> Vec<PathBuf> {
         let mut paths: Vec<PathBuf> = Vec::new();
 
         for path in &self.orderable_set.to_vec() {
             paths.push(path.to_path_buf());
         }
 
-        Some(paths)
+        paths
     }
 }
 
@@ -169,13 +168,14 @@ mod tests {
         let path_to_folder: &Path = Path::new("./test_files");
         let all_files = FileSet::new(path_to_folder);
 
-        let file_vec = all_files.to_vec().unwrap();
+        let file_vec = all_files.to_vec();
         let directory_location = file_vec[0].parent().unwrap();
 
-        assert_eq!(file_vec.len(), 8);
+        assert_eq!(file_vec.len(), 9);
+        assert!(file_vec.contains(&directory_location.join(".DS_Store")));
         assert!(file_vec.contains(&directory_location.join(".hidden_file_1.txt")));
         assert!(file_vec.contains(&directory_location.join(".hidden_file_2")));
-        assert!(file_vec.contains(&directory_location.join(".DS_Store")));
+        assert!(file_vec.contains(&directory_location.join(".symlink_to_gitkeep")));
         assert!(file_vec.contains(&directory_location.join("cat.doc")));
         assert!(file_vec.contains(&directory_location.join("directory_1")));
         assert!(file_vec.contains(&directory_location.join("directory_2")));
@@ -184,25 +184,24 @@ mod tests {
     }
 
     #[test]
-    fn visibility_type_filter_test() {
+    fn visibility_filter_test() {
         let path_to_folder: &Path = Path::new("./test_files");
         let mut all_files = FileSet::new(path_to_folder);
 
         let hidden_files = all_files
-            .filter(Filter::Visibility(Visibility::Hidden))
-            .to_vec()
-            .unwrap();
+            .filter(Filter::Visibility(VisibilityFilter::Hidden))
+            .to_vec();
         let directory_location = hidden_files[0].parent().unwrap();
 
-        assert_eq!(hidden_files.len(), 3);
+        assert_eq!(hidden_files.len(), 4);
         assert!(hidden_files.contains(&directory_location.join(".DS_Store")));
         assert!(hidden_files.contains(&directory_location.join(".hidden_file_1.txt")));
         assert!(hidden_files.contains(&directory_location.join(".hidden_file_2")));
+        assert!(hidden_files.contains(&directory_location.join(".symlink_to_gitkeep")));
 
         let visible_files = all_files
-            .filter(Filter::Visibility(Visibility::Visible))
-            .to_vec()
-            .unwrap();
+            .filter(Filter::Visibility(VisibilityFilter::Visible))
+            .to_vec();
 
         assert_eq!(visible_files.len(), 5);
         assert!(visible_files.contains(&directory_location.join("cat.doc")));
@@ -213,37 +212,36 @@ mod tests {
     }
 
     #[test]
-    fn directory_entry_type_filter_test() {
+    fn item_filter_test() {
         let path_to_folder: &Path = Path::new("./test_files");
         let mut all_files = FileSet::new(path_to_folder);
 
         let directories = all_files
-            .filter(Filter::Item(Item::Directory))
-            .to_vec()
-            .unwrap();
+            .filter(Filter::Item(ItemFilter::Directory))
+            .to_vec();
         let directory_location = directories[0].parent().unwrap();
 
         assert_eq!(directories.len(), 2);
         assert!(directories.contains(&directory_location.join("directory_1")));
         assert!(directories.contains(&directory_location.join("directory_2")));
 
-        let files = all_files.filter(Filter::Item(Item::File)).to_vec().unwrap();
+        let files = all_files.filter(Filter::Item(ItemFilter::File)).to_vec();
 
         assert_eq!(files.len(), 6);
+        assert!(files.contains(&directory_location.join(".DS_Store")));
         assert!(files.contains(&directory_location.join(".hidden_file_1.txt")));
         assert!(files.contains(&directory_location.join(".hidden_file_2")));
-        assert!(files.contains(&directory_location.join(".DS_Store")));
         assert!(files.contains(&directory_location.join("cat.doc")));
         assert!(files.contains(&directory_location.join("dog.txt")));
         assert!(files.contains(&directory_location.join("video.mov")));
+
+        let symlinks = all_files.filter(Filter::Item(ItemFilter::Symlink)).to_vec();
+
+        assert_eq!(symlinks.len(), 1);
+        assert!(symlinks.contains(&directory_location.join(".symlink_to_gitkeep")));
     }
 }
 
 // Ordering
 // Size (greater than / less than)
-// Reverse
-// Sort by file name, extension name
-// Files only
-// Directorys only
-// Extension name
-// Exclude
+// Sort test / reverse test / excluding
